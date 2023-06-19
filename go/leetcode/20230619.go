88e45e86bc8a86f3805689ebabbc0a9dd52c2c65
hoshitocat <hoshitocat@gmail.com>
2023-06-20 08:19:19 +0900

    :tada: corrent 20230619

--- go/leetcode/20230619.go
+++ go/leetcode/20230619.go
@@ -1,5 +1,22 @@
 package leetcode
 
+import "github.com/davecgh/go-spew/spew"
+
 func Run20230619() {
 	// https://leetcode.com/problems/find-the-highest-altitude/
+	spew.Dump(largestAltitude([]int{-5, 1, 5, 0, -7}))
+}
+
+func largestAltitude(gain []int) int {
+	var highest int
+	var current int
+	for _, g := range gain {
+		current += g
+
+		if highest < current {
+			highest = current
+		}
+	}
+
+	return highest
 }
