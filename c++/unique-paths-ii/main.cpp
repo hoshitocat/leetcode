1dc5e72b51d3bbab9840dd4c2a12e3248f483b4c
hoshitocat <hoshitocat@gmail.com>
2023-08-16 22:03:11 +0900

    :building_construction: WIP 2023/08/16

--- c++/unique-paths-ii/main.cpp
+++ c++/unique-paths-ii/main.cpp
@@ -6,7 +6,30 @@ using namespace std;
 class Solution {
   public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-      cout << "hoge" << endl;
+      vector<vector<int>> dp;
+      for (int i = 0; i < obstacleGrid.size(); i++) {
+        vector<int> val(obstacleGrid[i].size(), 0);
+        dp.push_back(val);
+      }
+
+
+
+      for (int i = 0; i < dp.size(); i++) {
+        for (int j = 0; j < dp[i].size(); j++) {
+          pair<int, int> left = {i, j - 1};
+          pair<int, int> up = {i - 1, j};
+          // WIP
+        }
+      }
+
+      // debug
+      for (int i = 0; i < obstacleGrid.size(); i++) {
+        for (int j = 0; j < obstacleGrid[i].size(); j++) {
+          cout << dp[i][j];
+        }
+        cout << endl;
+      }
+
       return 0;
     }
 };
