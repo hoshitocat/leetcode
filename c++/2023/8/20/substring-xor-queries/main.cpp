b3b68559ae7a7dc1766eb5178f8cdeeb5275ec96
hoshitocat <hoshitocat@gmail.com>
2023-08-21 22:13:24 +0900

    :tada: clear 2023/08/21

--- c++/2023/8/20/substring-xor-queries/main.cpp
+++ c++/2023/8/20/substring-xor-queries/main.cpp
@@ -1,62 +1,51 @@
 #include<iostream>
 #include<vector>
-#include<stack>
+#include<unordered_map>
 
 using namespace std;
 
 class Solution {
   public:
     vector<vector<int>> substringXorQueries(string s, vector<vector<int>>& queries) {
-      vector<vector<int>> result;
-      for (int i = 0; i < queries.size(); i++) {
-        int first = queries[i][0];
-        int second = queries[i][1];
-        int xored = first ^ second;
-        string b = toBinary(xored);
-        size_t index = s.find(b);
-        if (index == string::npos) {
-          result.push_back({-1, -1});
-          continue;
+      unordered_map<int, vector<int>> mp;
+      int len = s.length();
+      for (int i = 0; i < len; i++) {
+        int x = 0;
+        for (int j = 0; i+j < len && j < 32; j++) {
+          x = (x << 1) + (s[j+i] == '1');
+          if (mp.count(x) == 0) {
+            mp[x].push_back(i);
+            mp[x].push_back(i+j);
+          }
+
+          if (s[i] == '0') break;
         }
-
-        int left = static_cast<int>(index);
-        int right = static_cast<int>(b.size() - 1);
-
-        result.push_back({left, left+right});
       }
 
-      return result;
-    }
-
-  private:
-    string toBinary(int n) {
-      if (n == 0) return "0";
-
-      stack<string> s;
-      while (n != 0) {
-        s.push(n % 2 == 0 ? "0" : "1");
-        n /= 2;
+      vector<vector<int>> ans;
+      for (auto &i : queries) {
+        int xored = i[0] ^ i[1];
+        if (mp.count(xored))
+          ans.push_back(mp[xored]);
+        else
+          ans.push_back({-1, -1});
       }
-
-      string r;
-      while (!s.empty()) {
-        r += s.top();
-        s.pop();
-      }
-
-      return r;
+      return ans;
     }
 };
 
 int main() {
   Solution solution;
+
+  string s = "101101";
   vector<vector<int>> queries = {
-    {4, 2},
-    {3, 3},
+    {0, 5},
+    {1, 2},
   };
 
+
   vector<vector<int>> result;
-  result = solution.substringXorQueries("111010110", queries);
+  result = solution.substringXorQueries(s, queries);
   for (auto &r : result) {
     printf("{ %d, %d }\n", r[0], r[1]);
   }
