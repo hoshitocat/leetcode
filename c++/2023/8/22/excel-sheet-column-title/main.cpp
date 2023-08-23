c1349a93dcb7bed26c2f1843a7aedabef6bd7636
hoshitocat <hoshitocat@gmail.com>
2023-08-23 12:53:07 +0900

    :tada: clear 2023/08/22

--- c++/2023/8/22/excel-sheet-column-title/main.cpp
+++ c++/2023/8/22/excel-sheet-column-title/main.cpp
@@ -7,27 +7,12 @@ using namespace std;
 class Solution {
   public:
     string convertToTitle(int columnNumber) {
-      const int base = 26;
-      vector<string> titles = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
-
       string result = "";
-
-      int remain = columnNumber % base;
-      if (remain == 0) {
-        result = titles[base-1];
-      } else {
-        result = titles[remain-1];
-      }
-
-      while (0 < columnNumber) {
-        int quotient = columnNumber / base;
-        if (quotient <= base) {
-          result = titles[quotient-1] + result;
-        } else {
-          result = titles[base-1] + result;
-        }
-
-        columnNumber = quotient - base;
+      while (columnNumber > 0) {
+        columnNumber--;
+        char c = 'A' + columnNumber % 26;
+        result = c + result;
+        columnNumber /= 26;
       }
 
       return result;
@@ -37,7 +22,7 @@ class Solution {
 int main() {
   Solution solution;
 
-  vector<int> cases = {1, 28, 701, 1000};
+  vector<int> cases = {1, 28, 701, 702, 1000};
   for (auto &num : cases) {
     cout << solution.convertToTitle(num) << endl;
   }
