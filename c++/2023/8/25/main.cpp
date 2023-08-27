42b2a1dfb45f80f15b584d84c23f022739edc511
hoshitocat <hoshitocat@gmail.com>
2023-08-27 12:58:29 +0900

    :tada: resolve 1D array 2023/08/27

--- c++/2023/8/25/main.cpp
+++ c++/2023/8/25/main.cpp
@@ -7,35 +7,29 @@ using namespace std;
 class Solution {
   public:
     bool isInterleave(string s1, string s2, string s3) {
-      int n = s1.size();
-      int m = s2.size();
+      int m = s1.size();
+      int n = s2.size();
+      int l = s3.size();
 
-      if ((n+m) != s3.size()) return false;
+      if ((n+m) != l) return false;
+      if (m < n) return isInterleave(s2, s1, s3);
 
-      n++;
-      m++;
+      vector<bool> dp(n+1, false);
+      dp[0] = true;
 
-      vector<vector<bool>> dp(n, vector<bool>(m, false));
-      dp[0][0] = true;
-
-      for (int i = 1; i < n; i++) {
-        dp[i][0] = dp[i-1][0] && (s1[i-1] == s3[i-1]);
+      for (int j = 1; j <= n; j++) {
+        dp[j] = dp[j - 1] && s2[j - 1] == s3[j - 1];
       }
 
-      for (int j = 1; j < m; j++) {
-        dp[0][j] = dp[0][j-1] && (s2[j-1] == s3[j-1]);
-      }
 
-      for (int i = 1; i < n; i++) {
-        for (int j = 1; j < m; j++) {
-          bool isMatchS1 = dp[i-1][j] && (s1[i-1] == s3[i+j - 1]);
-          bool isMatchS2 = dp[i][j-1] && (s2[j-1] == s3[i+j - 1]);
-
-          dp[i][j] = isMatchS1 || isMatchS2;
+      for (int i = 1; i <= m; i++) {
+        dp[0] = dp[0] && s1[i - 1] == s3[i - 1];
+        for (int j = 1; j <= n; j++) {
+          dp[j] = (dp[j] && s1[i - 1] == s3[i + j - 1]) || (dp[j - 1] && s2[j - 1] == s3[i + j - 1]);
         }
       }
 
-      return dp[n-1][m-1];
+      return dp[n];
     }
 };
 
