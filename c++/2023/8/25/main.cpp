0efafc469cce92d3e1114b0b0f75677127fae4d2
hoshitocat <hoshitocat@gmail.com>
2023-08-29 23:23:15 +0900

    :building_construction: WIP 2023/08/29

--- c++/2023/8/25/main.cpp
+++ c++/2023/8/25/main.cpp
@@ -1,51 +1,34 @@
 // https://leetcode.com/problems/interleaving-string/
 #include<iostream>
 #include<vector>
-#include<unordered_map>
 
 using namespace std;
 
-const int null = 2;
-
 class Solution {
-  string s1, s2, s3;
-  vector<vector<int>> memo;
-
   public:
     bool isInterleave(string s1, string s2, string s3) {
-      this->s1 = s1;
-      this->s2 = s2;
-      this->s3 = s3;
+      int m = s1.size();
+      int n = s2.size();
+      int l = s3.size();
+      if (m+n != l) return false;
 
-      int a = s1.size();
-      int b = s2.size();
-      int c = s3.size();
+      if (m < n) isInterleave(s2, s1, s3);
 
-      if (a+b != c) return false;
+      vector<bool> dp(n+1, false);
+      dp[0] = true;
 
-      memo = vector<vector<int>>(a+1, vector<int>(b+1, null));
-
-      return helper(0, 0, 0);
-    }
-
-  private:
-    bool helper(int a, int b, int c) {
-      if (c == s3.size()) return true;
-
-      int m = memo[a][b];
-      if (m != null) return m;
-
-      bool ans = false;
-      if (a < s1.size() && s1[a] == s3[c]) {
-        ans = ans || helper(a+1, b, c+1);
+      for (int j = 1; j < n+1; j++) {
+        dp[j] = dp[j-1] && s2[j-1] == s3[j-1];
       }
 
-      if (b < s2.size() && s2[b] == s3[c]) {
-        ans = ans || helper(a, b+1, c+1);
+      for (int i = 1; i < m+1; i++) {
+        dp[0] = dp[0] && s1[i-1] == s3[i-1];
+        for (int j = 1; j < n+1; j++) {
+          dp[j] = (dp[j] && s1[j-1] == s3[i+j-1]) || (dp[j-1] && s2[j-1] == s3[i+j-1]);
+        }
       }
 
-      memo[a][b] = ans;
-      return ans;
+      return dp[n];
     }
 };
 
