b7bc90aca1bc131c1c9ee1d6ab724f265dc49012
hoshitocat <hoshitocat@gmail.com>
2023-08-31 22:17:53 +0900

    :building_construction: init 2023/08/31

---
+++ c++/2023/8/31/main.cpp
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/merge-sorted-array/?envType=study-plan-v2&envId=top-interview-150
+
+#include<iostream>
+
+using namespace std;
+
+class Solution {
+  public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    }
+};
+
+int main() {
+  Solution solution;
+}
