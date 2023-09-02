fa73f2060e3bee227dcddabf0f41360b44ac48e8
hoshitocat <hoshitocat@gmail.com>
2023-09-02 15:38:40 +0900

    :tada: answer 2023/09/02

--- go/leetcode/2023/9/1/main.go
+++ go/leetcode/2023/9/1/main.go
@@ -1,7 +1,38 @@
+// https://leetcode.com/problems/remove-element/?envType=study-plan-v2&envId=top-interview-150
 package main
 
-func merge(nums1 []int, m int, nums2 []int, n int)  {
+import "fmt"
+
+func removeElement(nums []int, val int) int {
+	var i = 0;
+	for _, n := range nums {
+		if n != val {
+			nums[i] = n
+			i++
+		}
+	}
+
+	nums = nums[:i]
+	return len(nums)
 }
 
 func main() {
+	cases := []struct {
+		nums []int
+		val int
+	}{
+		{
+			nums: []int{3, 2, 2, 3},
+			val: 3,
+		},
+		{
+			nums: []int{0,1,2,2,3,0,4,2},
+			val: 2,
+		},
+	}
+
+	for _, c := range cases {
+		c := c
+		fmt.Println(removeElement(c.nums, c.val))
+	}
 }
