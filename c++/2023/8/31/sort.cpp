cc57d5a9b6a533277d4fd051856b7d1dc003eeb3
hoshitocat <hoshitocat@gmail.com>
2023-09-02 15:04:51 +0900

    :recycle: clear 2023/08/31 using two pointers

--- c++/2023/8/31/sort.cpp
+++ c++/2023/8/31/sort.cpp
@@ -10,20 +10,12 @@ class Solution {
   public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
       if (n == 0) return;
-      int i = m - 1;
-      int j = n - 1;
-      int k = m + n - 1;
 
-      while (j >= 0) {
-        if (i >= 0 && nums1[i] >= nums2[j]) {
-          nums1[k] = nums1[i];
-          i--;
-        } else {
-          nums1[k] = nums2[j];
-          j--;
-        }
-        k--;
+      for (int j = 0; j < n; j++) {
+        nums1[m+j] = nums2[j];
       }
+
+      sort(nums1.begin(), nums1.end());
     }
 };
 
